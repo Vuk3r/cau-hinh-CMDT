ffd0c707e7b6895b5fbadd0108f670bd5ada5aa2
Ld4vicl <connguathanhtroia@gmail.com>
2025-12-13 19:31:14 +0700

    HSRP + OSPF + DHCP tang 1,2,3

--- firewall.sh
+++ firewall.sh
@@ -3,26 +3,43 @@ en
 conf t
 
 interface Gi1/1
+nameif FW1-EDGE1
 ip address 10.43.231.253 255.255.255.252
 no shutdown
 exit
 
 interface Gi1/2 
+nameif FW1-EDGE2
 ip address 10.43.232.253 255.255.255.252
 no shutdown
 exit
 
-interface Gi1/3 
+interface Gi1/3
+nameif FW1-CORE1
+security-level 50
 ip address 10.43.221.254 255.255.255.252
 no shutdown
 exit
 
-interface Gi1/4 
+interface Gi1/4
+nameif FW1-CORE2
+security-level 50
 ip address 10.43.223.254 255.255.255.252
 no shutdown
 exit
 
-! ======== FIREWALL 2 ======== !  
+router ospf 1
+router-id 231.231.231.231
+network 10.43.231.252 255.255.255.252 area 0
+network 10.43.232.252 255.255.255.252 area 0
+network 10.43.221.252 255.255.255.252 area 0
+network 10.43.223.252 255.255.255.252 area 0
+
+!!!! FIX KHI CAU HINH HSRP !!!!
+ip route 10.43.0.0 255.255.0.0 10.43.221.253
+
+
+! ======== FIREWALL 2 ======== ! 
 en
 conf t
 
--- tmp.sh
+++ tmp.sh
@@ -1,138 +1,52 @@
 
-! =============== Distribute Tang 4 =============== !
-!swl3-1
-en
-conf t
-ip routing
+! ==== HSRP - DIS 3 ==== !
 
-vlan 41
-name VLAN41-letan
-vlan 42
-name VLAN42-anninh
-vlan 43
-name VLAN43-kho
-vlan 51
-name VLAN51-hanhchinh
-vlan 52
-name VLAN52-ketoan
-vlan 53
-name VLAN53-phapche
-vlan 61
-name VLAN61-sales 
-vlan 62
-name VLAN62-mkt
-vlan 63
-name VLAN61-studio
+! = TANG 1 = !
+interface vlan 11
+standby 11 ip 10.43.16.254
+standby 11 priority 110
+standby 11 preempt
 
+interface vlan 12
+standby 11 ip 10.43.17.254
+standby 11 priority 110
+standby 11 preempt
 
-interface vlan 41
-ip address 10.43.48.1 255.255.255.0
-no shutdown
+interface vlan 13
+standby 11 ip 10.43.18.254
+standby 11 priority 110
+standby 11 preempt
 
-interface vlan 42
-ip address 10.43.49.1 255.255.255.0
-no shutdown
+! = TANG 2 = !
 
-interface vlan 43
-ip address 10.43.50.1 255.255.255.0
-no shutdown
+interface vlan 21
+standby 11 ip 10.43.24.254
+standby 11 priority 100
+standby 11 preempt
 
-interface vlan 51
-ip address 10.43.56.1 255.255.255.0
-no shutdown
+interface vlan 22
+standby 11 ip 10.43.25.254
+standby 11 priority 100
+standby 11 preempt
 
-interface vlan 52
-ip address 10.43.57.1 255.255.255.0
-no shutdown
+interface vlan 23
+standby 11 ip 10.43.26.254
+standby 11 priority 100
+standby 11 preempt
 
-interface vlan 53
-ip address 10.43.58.1 255.255.255.0
-no shutdown
+! = TANG 3 = !
 
-interface vlan 61
-ip address 10.43.64.1 255.255.255.0
-no shutdown
+interface vlan 31
+standby 11 ip 10.43.32.254
+standby 11 priority 120
+standby 11 preempt
 
-interface vlan 62
-ip address 10.43.65.1 255.255.255.0
-no shutdown
+interface vlan 32
+standby 11 ip 10.43.33.254
+standby 11 priority 120
+standby 11 preempt
 
-interface vlan 63
-ip address 10.43.66.1 255.255.255.0
-no shutdown
-
-!trunk
-interface fa0/3
-switchport trunk encapsulation dot1q
-switchport mode trunk
-
-switchport trunk allowed vlan 41,42,43
-no shutdown
-!trunk
-interface fa0/4
-switchport trunk encapsulation dot1q
-switchport mode trunk
-switchport trunk allowed vlan 51,52,53
-no shutdown
-!trunk
-interface fa0/5
-switchport trunk encapsulation dot1q
-switchport mode trunk
-switchport trunk allowed vlan 61,62,63
-no shutdown
-
-!routing 
-interface fa0/1
-no switchport
-ip address 10.43.207.253 255.255.255.252
-no shutdown
-!routing 
-ip routing
-interface fa0/2
-no switchport
-ip address 10.43.208.253 255.255.255.252
-no shutdown
-
-router ospf 1
-! toan bo mang o tang 4 -> 6 - them vao de day cho router - mo rong wildcard de khong phai khai bao dai dong
-! full vlan tang 4
-network 10.43.48.0 0.0.3.255 area 0   
-! full vlan tang 5
-network 10.43.56.0 0.0.3.255 area 0   
-! full vlan tang 6
-network 10.43.64.0 0.0.3.255 area 0 
-
-network 10.43.207.252 0.0.0.3 area 0
-network 10.43.208.252 0.0.0.3 area 0
-
-! gui ra mang internet
-ip route 0.0.0.0 0.0.0.0 10.43.207.254
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-! DHCP - ip helper for DHCP(10.43.150.2)
-interface vlan 41
-ip helper-address 10.43.150.2
-interface vlan 42
-ip helper-address 10.43.150.2
-interface vlan 43
-ip helper-address 10.43.150.2
-
-interface vlan 51
-ip helper-address 10.43.150.2
-interface vlan 52
-ip helper-address 10.43.150.2
-interface vlan 53
-ip helper-address 10.43.150.2
-
-interface vlan 61
-ip helper-address 10.43.150.2
-interface vlan 62
-ip helper-address 10.43.150.2
-interface vlan 63
-ip helper-address 10.43.150.2
-
-
-
-
-
-
+interface vlan 33
+standby 11 ip 10.43.34.254
+standby 11 priority 120
+standby 11 preempt
